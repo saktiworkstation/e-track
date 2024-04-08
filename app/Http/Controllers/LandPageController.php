21de6c5fa9104b87bb948af5d6f8382173b8b9df
saktiworkstation <saktiworkstation@gmail.com>
2024-04-08 12:44:00 +0700

    Display tickets data

--- app/Http/Controllers/LandPageController.php
+++ app/Http/Controllers/LandPageController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class LandPageController extends Controller
 {
     public function index(){
-        return view('landing-page');
+        return view('landing-page', [
+            'tickets' => Ticket::orderBy('created_at', 'asc')->get()
+        ]);
     }
 }
