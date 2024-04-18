c01047351e4de31faca93a92b116479ad79ce065
saktiworkstation <saktiworkstation@gmail.com>
2024-04-18 22:34:15 +0700

    Fill in the fields in the migration table

--- database/migrations/2024_04_18_070043_create_events_table.php
+++ database/migrations/2024_04_18_070043_create_events_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->dateTime('published_at');
             $table->timestamps();
         });
     }
