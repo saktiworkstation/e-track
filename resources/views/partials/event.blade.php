f1194d14e60898426ca79e00895251140f3dffc8
saktiworkstation <saktiworkstation@gmail.com>
2024-04-19 14:35:56 +0700

    Create partials/events page

---
+++ resources/views/partials/event.blade.php
@@ -0,0 +1,25 @@
+<div class="container py-3" id="tickets">
+    <header>
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal text-body-emphasis">Events</h1>
+            <p class="fs-5 text-body-secondary">Always check the latest news and events, don't miss any interesting
+                things from us!.</p>
+        </div>
+    </header>
+
+    <main>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="card">
+                <div class="card-header">
+                    Quote
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                        <p>A well-known quote, contained in a blockquote element.</p>
+                        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source
+                                Title</cite></footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+</div>
