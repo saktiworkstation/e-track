59526c7433c12dc719c31c2950350083640e5404
saktiworkstation <saktiworkstation@gmail.com>
2024-04-19 14:56:01 +0700

    repair the card part

--- resources/views/partials/event.blade.php
+++ resources/views/partials/event.blade.php
@@ -9,16 +9,32 @@
 
     <main>
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-            <div class="card">
-                <div class="card-header">
-                    Quote
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        Quote
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source
+                                    Title</cite></footer>
+                        </blockquote>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p>A well-known quote, contained in a blockquote element.</p>
-                        <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source
-                                Title</cite></footer>
-                    </blockquote>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        Quote
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source
+                                    Title</cite></footer>
+                        </blockquote>
+                    </div>
                 </div>
             </div>
         </div>
