76c83b29038e683b1b73c9e55a33760a07a9541e
saktiworkstation <saktiworkstation@gmail.com>
2024-04-19 16:00:26 +0700

    sending event data to the landing page

--- app/Http/Controllers/LandPageController.php
+++ app/Http/Controllers/LandPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,8 @@ class LandPageController extends Controller
 {
     public function index(){
         return view('landing-page', [
-            'tickets' => Ticket::orderBy('created_at', 'asc')->paginate(6)
+            'tickets' => Ticket::orderBy('created_at', 'asc')->paginate(6),
+            'events' => Event::latest()->get()
         ]);
     }
 }
