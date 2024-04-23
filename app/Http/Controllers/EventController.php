35a428cb751e5a6c8bf6ce12a97d06c0c0ef632d
saktiworkstation <saktiworkstation@gmail.com>
2024-04-23 20:07:58 +0700

    Add tittle variable

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,6 +14,7 @@ class EventController extends Controller
     {
         return view('dashboard.event.index', [
             'events' => Event::latest()->get(),
+            'title' => 'Events',
         ]);
     }
 
