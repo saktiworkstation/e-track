f4592ab548bbb833708450c9656f264af8639971
saktiworkstation <saktiworkstation@gmail.com>
2024-04-25 21:43:30 +0700

    Send the notifications data to view

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -14,6 +14,7 @@ class NotificationController extends Controller
     {
         return view('dashboard.notification.index',[
             'title' => 'Notification',
+            'notifications' => Notification::latest()->get(),
         ]);
     }
 
