60ce15cd3280a705c8c2fd0514cd7f7ebb71ecb8
saktiworkstation <saktiworkstation@gmail.com>
2024-04-25 21:36:54 +0700

    Filled the index function

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -12,7 +12,9 @@ class NotificationController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.notification.index',[
+            'title' => 'Notification',
+        ]);
     }
 
     /**
