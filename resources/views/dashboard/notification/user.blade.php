731555636b318619219faed86d0d1343914cac77
saktiworkstation <saktiworkstation@gmail.com>
2024-04-27 20:42:39 +0700

    Create message card

--- resources/views/dashboard/notification/user.blade.php
+++ resources/views/dashboard/notification/user.blade.php
@@ -11,4 +11,31 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+
+    <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <div class="d-flex">
+                        <div class="p-2 w-100"><a href="#" class="btn btn-primary">Go To the link of message</a></div>
+                        <div class="p-2 flex-shrink-1"><a href="#" class="btn btn-danger">Delete</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <div class="d-flex">
+                        <div class="p-2 w-100"><a href="#" class="btn btn-primary">Go To the link of message</a></div>
+                        <div class="p-2 flex-shrink-1"><a href="#" class="btn btn-danger">Delete</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
