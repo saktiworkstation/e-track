f4a946a423eb6b501581458a8e56140676a04149
saktiworkstation <saktiworkstation@gmail.com>
2024-05-03 18:39:23 +0700

    Send users data to dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\LandPageController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\TicketController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,7 +29,9 @@ Route::get('/register', [AuthController::class, 'register'])->middleware('guest'
 Route::post('/register', [AuthController::class, 'store']);
 
 Route::get('/dashboard', function () {
-    return view('dashboard.index');
+    return view('dashboard.index', [
+        'users' => User::latest()->all(),
+    ]);
 })->middleware('auth');
 
 //* user tiket
