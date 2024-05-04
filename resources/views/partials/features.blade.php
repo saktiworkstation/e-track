1f85b74a223696d3ff899e1e0203b5b9f9164ebb
saktiworkstation <saktiworkstation@gmail.com>
2024-05-04 13:56:53 +0700

    Added feature content

--- resources/views/partials/features.blade.php
+++ resources/views/partials/features.blade.php
@@ -3,75 +3,30 @@
 
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
         <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#bootstrap" />
-            </svg>
             <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
+                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Notifications</h3>
+                <p>Notification system that informs users about ticket purchase status, payment confirmation and other
+                    important changes.</p>
             </div>
         </div>
         <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#cpu-fill" />
-            </svg>
             <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
+                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Ticket Purchase</h3>
+                <p>Users can select the events they want to participate in and purchase tickets online through this
+                    platform</p>
             </div>
         </div>
         <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#calendar3" />
-            </svg>
             <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
+                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Ticket Usage</h3>
+                <p>Users can use their purchased tickets to attend related events. The event owner will then confirm the
+                    use of the ticket.</p>
             </div>
         </div>
         <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#home" />
-            </svg>
             <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
-            </div>
-        </div>
-        <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#speedometer2" />
-            </svg>
-            <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
-            </div>
-        </div>
-        <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#toggles2" />
-            </svg>
-            <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
-            </div>
-        </div>
-        <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#geo-fill" />
-            </svg>
-            <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
-            </div>
-        </div>
-        <div class="col d-flex align-items-start">
-            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em">
-                <use xlink:href="#tools" />
-            </svg>
-            <div>
-                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Featured title</h3>
-                <p>Paragraph of text beneath the heading to explain the heading.</p>
+                <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">Account Management</h3>
+                <p>Facility to manage user accounts.</p>
             </div>
         </div>
     </div>
