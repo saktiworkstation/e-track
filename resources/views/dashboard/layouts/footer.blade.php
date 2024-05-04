0dfb2dc0c7d5a8061a7810412d827cc186542886
saktiworkstation <saktiworkstation@gmail.com>
2024-05-04 14:12:20 +0700

    Create footer for dashboard

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <footer class="py-2 my-2">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        </ul>
+        <p class="text-center text-body-secondary">&copy; 2024 create By <a href="https://github.com/saktiworkstation"
+                target="_blank" class="text-decoration-none">saktiworkstation</a>, Made by spirit 🧙‍♀️</p>
+    </footer>
+</div>
--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -42,6 +42,8 @@
         </div>
     </div>
 
+    @include('dashboard.layouts.footer')
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
